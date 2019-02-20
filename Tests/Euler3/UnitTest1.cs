fe6f52ca5263bc7dcfd68d117a7e804b8b937100
Brian Jones <ba.jones@me.com>
2019-02-20 04:26:37 +0000

    Project restructure

---
+++ Tests/Euler3/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euler3.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
