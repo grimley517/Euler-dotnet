f3f79a501fc9225bc5825bb0b8051047083da07a
Brian Jones <grimley517@users.noreply.github.com>
2019-02-23 11:44:22 +0000

    added test to thirty

--- Tests/Euler1/euler1Tests.cs
+++ Tests/Euler1/euler1Tests.cs
@@ -26,6 +26,19 @@ namespace euler1.tests
             int actual = (int) prob.Resolve();
             Assert.AreEqual(expected, actual);
         }
+        
+        [TestMethod]
+        public void TestToThirty()
+        {
+            // threes: 3,6,9,12,15, 18, 21, 24, 27 => 135
+            // fives: 5, 10, 15, 20, 25,=> 75
+            // Subtotal => 210
+            // Remove 15 counted twice => 195
+            int expected = 195; 
+            var prob = new Problem1(30);
+            int actual = (int) prob.Resolve();
+            Assert.AreEqual(expected, actual);
+        }
 
                 [TestMethod]
         public void TestToThousand()
